6bdb5cbd86dd58054624071ca8dc4c3b72ef65e5
cleeRprogramming <leechyi.tokyo@gmail.com>
2014-06-07 07:39:45 +0700

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -2,7 +2,6 @@ plot3 <- function(){
 	data <- read.table("data/household_power_consumption.txt", header=T, sep=";")
 	data$Date <- as.Date(data$Date, "%d/%m/%Y")
 	final.data <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
-	final.data$Global_active_power <- as.numeric(as.character(final.data$Global_active_power))
 	final.data$DateTime <- as.POSIXct(paste(final.data$Date, final.data$Time))
 	plot(final.data$DateTime, final.data$Sub_metering_1, type='l', ylab="Energy sub metering", xlab="")
 	lines(final.data$DateTime, final.data$Sub_metering_2, col="red", type='l')
